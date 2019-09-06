92d75c2e6d378d9cab7427d97e0812963ca7c337
Ubuntu <ubuntu@ip-172-31-18-135.ec2.internal>
2019-09-06 05:47:01 +0000

    new commit

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import flask
 import os
+import requests
+import json
+import random
+import requests_oauthlib
 
-app = flask.Flask(__name__)
+# app = flask.Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return flask.render_template("index.html")
+# @app.route('/')
+# def index():
+  
+  
+### genius set up....formatting in index file?    
+url = "https://api.genius.com/search?q=Kehlani"
     
-app.run(
-    port=int(os.getenv('PORT', 8080)),
-    host=os.getenv('IP','0.0.0.0')
-    )
+    
+my_headers = {
+    "Authorization": "Bearer iIUzpgQ_jwrnzWcGPns32mdgaLcLF_KMCZxXydljLb3QLF24GSO1zq38e2uveLY4"
+    }
+    
+response = requests.get(url, headers=my_headers)
+json_body = response.json()
+song = json_body["response"]["hits"][0]["result"]["full_title"])
+pic = json_body["response"]["hits"][0]["result"]['header_image_url']
+artist = json_body["response"]["hits"][0]["result"]['primary_artist']['name']
