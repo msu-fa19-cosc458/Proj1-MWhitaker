85ea02177eadb18f03796221f15ba752b5531182
Ubuntu <ubuntu@ip-172-31-18-135.ec2.internal>
2019-09-06 06:06:45 +0000

    working twitter api

--- app.py
+++ app.py
@@ -12,6 +12,26 @@ import requests_oauthlib
 # def index():
   
   
+##twitter api...hopefully
+
+
+twitt_url = "https://api.twitter.com/1.1/search/tweets.json?q=kehlanimusic"
+
+oauth = requests_oauthlib.OAuth1(
+    "eBuX7hYeHDVpIEwgA5cwwAz8P", 
+    "mEyLbMPJvLS2YAcxFJLk4yT44uhsgLsK5O4kr1w66XwtznEgXv",
+    "1167289280638726144-s4dO5VvLl5lFvXNMn39aG1vjY2NXuZ",
+    "ijHOBxlSLDU8jwnLRbMxlCqW6c5FCOkMAQDoqN0MpOvOo"
+)
+## not sure if this will actually show the tweets, gives links for tweets
+response = requests.get(twitt_url, auth=oauth)
+json_body = response.json()
+#print(json.dumps(json_body, indent=2))
+
+tweetfeed = json_body['statuses'][14]['text']
+
+print(tweetfeed)
+  
 ### genius set up....formatting in index file?    
 url = "https://api.genius.com/search?q=Kehlani"
     
@@ -22,6 +42,6 @@ my_headers = {
     
 response = requests.get(url, headers=my_headers)
 json_body = response.json()
-song = json_body["response"]["hits"][0]["result"]["full_title"])
+song = json_body["response"]["hits"][0]["result"]["full_title"]
 pic = json_body["response"]["hits"][0]["result"]['header_image_url']
 artist = json_body["response"]["hits"][0]["result"]['primary_artist']['name']
